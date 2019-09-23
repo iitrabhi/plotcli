8ba3c813ba27e3ba34628f1c9cec2c00398aee75
Abhinav Gupta <iitrabhi@gmail.com>
2019-09-23 20:05:01 +0530

    update status field

--- plotcli/__about__.py
+++ plotcli/__about__.py
@@ -1,9 +1,9 @@
 __version__ = "1.1"
 __author__ = "Abhinav Gupta"
 __author_email__ = "iitrabhi@gmail.com"
-__copyright__ = "Copyright (c) 2019-2020 {} <{}>".format(
-    __author__, __author_email__
-)
+__copyright__ = "Copyright (c) 2019-2020 {} <{}>".format(__author__, __author_email__)
 __website__ = "https://github.com/iitrabhi/plotcli"
 __license__ = "License :: OSI Approved :: MIT License"
-__status__ = "Development Status :: Developement/Un-stable"
+__status__ = (
+    "Development Status :: 4 - Beta"
+)  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
